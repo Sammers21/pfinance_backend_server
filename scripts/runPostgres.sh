a4808b58c9e3954b553dccbfd7fc7628f419b4d0
Pavel Drankov <titantins@gmail.com>
2017-06-18 21:49:44 +0300

    add container initialization before app launching

--- scripts/runPostgres.sh
+++ scripts/runPostgres.sh
@@ -2,4 +2,8 @@
 if [ ! "$(sudo docker ps -q -f name=postgres)" ]; then
     # run your container
     sudo docker run --publish 5432:5432 --name postgres -d fpostgres
+    while ! nc -z localhost 5432; do
+        sleep 0.1 # wait for 1/10 of the second before check again
+        echo wait
+    done
 fi
--- src/main/java/ru/xidv/drankov/fassist/FassistApplication.java
+++ src/main/java/ru/xidv/drankov/fassist/FassistApplication.java
@@ -1,12 +1,8 @@
 package ru.xidv.drankov.fassist;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ru.xidv.drankov.fassist.beans.DockerStarter;
-
-import java.io.File;
 
 @EnableJpaRepositories
 @SpringBootApplication
@@ -14,8 +10,6 @@ public class FassistApplication {
 
 
     public static void main(String[] args) {
-        DockerStarter.start();
-        System.out.println("kek");
         SpringApplication.run(FassistApplication.class, args);
     }
 
