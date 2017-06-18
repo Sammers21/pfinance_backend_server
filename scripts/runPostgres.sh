6110bd38579964257a85aaed9b2094c43f03fcc6
Pavel Drankov <titantins@gmail.com>
2017-06-18 22:41:51 +0300

    change image build settings

--- scripts/runPostgres.sh
+++ scripts/runPostgres.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 if [ ! "$(sudo docker ps -q -f name=postgres)" ]; then
     # run your container
-    sudo docker run --publish 5432:5432 --name postgres -d fpostgres
-    while ! nc -z localhost 5432; do
-        sleep 0.1 # wait for 1/10 of the second before check again
-        echo wait
-    done
+    sudo docker run --publish 5432:5432 --name postgres -d sammers/fpostgres
 fi
