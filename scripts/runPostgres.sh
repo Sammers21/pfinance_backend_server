0923d452d0e8ea0e0cebb49316314d8f4b8d4082
Pavel Drankov <titantins@gmail.com>
2017-06-18 22:52:14 +0300

    adde debug info

--- scripts/runPostgres.sh
+++ scripts/runPostgres.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # run your container
-sudo docker run --publish 5432:5432 --name postgres -d sammers/fpostgres
+sudo docker ps -a
+sudo docker run --publish 5432:5432 --name postgres -d sammers/fpostgres:lastest
