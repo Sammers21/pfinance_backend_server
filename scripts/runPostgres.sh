0e964c0305ff52b95565167b99ee04eb7bff43e5
Pavel Drankov <titantins@gmail.com>
2017-06-18 22:49:16 +0300

    del some useless lines

--- scripts/runPostgres.sh
+++ scripts/runPostgres.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-if [ ! "$(sudo docker ps -q -f name=postgres)" ]; then
-    # run your container
-    sudo docker run --publish 5432:5432 --name postgres -d sammers/fpostgres
-fi
+# run your container
+sudo docker run --publish 5432:5432 --name postgres -d sammers/fpostgres
